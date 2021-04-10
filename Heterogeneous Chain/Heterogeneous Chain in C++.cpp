800b325ddd0275ff9c99770350d101c328b51570
Ewan-K <2574017297@qq.com>
2021-04-10 10:34:14 +0800

    Heterogeneous Chain

---
+++ Heterogeneous Chain/Heterogeneous Chain in C++.cpp
@@ -0,0 +1,322 @@
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+typedef class Node *List;
+//Base
+class Node
+{
+public:
+    List Next;
+
+public:
+    Node(){};
+    virtual ~Node(){};
+    virtual void printData() const;     //仅在派生类中定义
+    virtual int tellType() const;       //判断数据类型
+    virtual void Output();              //遍历链表输出
+    virtual List FindKth(int K);        //查找并指向第K个元素
+    virtual List Insert(int K, List X); //在第K个位置插入元素X（已初始化的对象）
+    virtual List DeleteKth(int K);      //删除第K个位置的元素
+    virtual List Reverse();             //逆置链表
+    virtual List Merge(List L1);        //合并链表，将this追加到L1后
+    virtual List sortByType();          //按值的类型排序（先INT，再DOUBLE，后CHAR）
+    virtual List Destroy();             //销毁链表
+};
+//Derived
+template <class T>
+class NodeS : public Node
+{
+private:
+    T Data;
+
+public:
+    NodeS(T Data, List Next) : Data(Data)
+    {
+        this->Next = Next;
+    };
+    ~NodeS(){};
+    void printData() const
+    {
+        cout << Data << endl;
+    };
+    int tellType() const
+    {
+        if (typeid(Data) == typeid(int))
+            return 1;
+        else if (typeid(Data) == typeid(double))
+            return 2;
+        else
+            return 3;
+    };
+    void Output();              //遍历链表输出
+    List FindKth(int K);        //查找并指向第K个元素
+    List Insert(int K, List X); //在第K个位置插入元素X（已初始化的对象）
+    List DeleteKth(int K);      //删除第K个位置的元素
+    List Reverse();             //逆置链表
+    List Merge(List L1);        //合并链表，将this追加到L1后
+    List sortByType();          //按值的类型排序（先INT，再DOUBLE，后CHAR）
+    List Destroy();             //销毁链表
+};
+
+int main()
+{
+    //建立链表
+    NodeS<int> L1(-1, nullptr); //必须用一个对象节点做头结点
+    NodeS<int> L2(-2, nullptr);
+    List PtrL1 = &L1, PtrL2 = &L2;
+    //插入
+    NodeS<int> I1(1, nullptr);
+    NodeS<int> I2(2, nullptr);
+    NodeS<double> D1(2020.11, nullptr);
+    NodeS<double> D2(2020.22, nullptr);
+    NodeS<char> C1('a', nullptr);
+    NodeS<char> C2('b', nullptr);
+
+    PtrL1 = PtrL1->Insert(1, &I1);
+    PtrL1 = PtrL1->Insert(2, &I2);
+    PtrL1 = PtrL1->Insert(3, &D1);
+    PtrL2 = PtrL2->Insert(1, &C1);
+    PtrL2 = PtrL2->Insert(2, &C2);
+    PtrL2 = PtrL2->Insert(3, &D2);
+    //输出
+    cout << "插入后的结果：" << endl;
+    cout << "PtrL1中元素：" << endl;
+    PtrL1->Output();
+    cout << "PtrL2中元素：" << endl;
+    PtrL2->Output();
+
+    cout << endl;
+
+    // //删除
+    // PtrL1 = PtrL1->DeleteKth(2);
+    // PtrL2 = PtrL2->DeleteKth(2);
+    // //输出
+    // cout << "删除后的结果：" << endl;
+    // cout << "PtrL1中元素：" << endl;
+    // PtrL1->Output();
+    // cout << "PtrL2中元素：" << endl;
+    // PtrL2->Output();
+
+    // cout << endl;
+
+    // //逆置
+    // PtrL1 = PtrL1->Reverse();
+    // PtrL2 = PtrL2->Reverse();
+    // //输出
+    // cout << "逆置后的结果：" << endl;
+    // cout << "PtrL1中元素：" << endl;
+    // PtrL1->Output();
+    // cout << "PtrL2中元素：" << endl;
+    // PtrL2->Output();
+
+    // cout << endl;
+
+    //合并
+    PtrL1 = PtrL1->Merge(PtrL2);
+    //输出
+    cout << "合并后的结果：" << endl;
+    cout << "PtrL1中元素：" << endl;
+    PtrL1->Output();
+    // PtrL2->Output(); //这里用PtrL2输出结果相同
+    cout << endl;
+
+    //排序
+    PtrL1 = PtrL1->sortByType();
+    //输出
+    cout << "排序后的结果：" << endl;
+    cout << "PtrL1中元素：" << endl;
+    PtrL1->Output();
+
+    cout << endl;
+
+    //销毁
+    PtrL1 = PtrL1->Destroy();
+    //输出
+    cout << "销毁后的结果：" << endl;
+    cout << "PtrL1中元素：" << endl;
+    PtrL1->Output();
+
+    getchar();
+    getchar();
+}
+
+template <class T>
+void NodeS<T>::Output()
+{
+    List p = this;
+    if (!p)
+    {
+        cout << "链表为空！" << endl;
+        return;
+    }
+    else
+    {
+        while (p)
+        {
+            cout << "该节点值为: ";
+            p->printData();
+            cout << endl;
+            p = p->Next;
+        }
+        return;
+    }
+}
+
+template <class T>
+List NodeS<T>::FindKth(int K)
+{
+    List L = this;
+    int i = 1;
+    while (L && i < K)
+    {
+        L = L->Next;
+        i++;
+    }
+    if (i == K)
+        return L;
+    else
+    {
+        cout << "K位置无法查" << endl;
+        return nullptr;
+    }
+}
+
+template <class T>
+List NodeS<T>::Insert(int K, List X)
+{
+    List p, l;
+    if (K == 1)
+    {
+        p = X;
+        p->Next = this;
+        return p;
+    }
+    l = FindKth(K - 1);
+    if (l)
+    {
+        p = X;
+        p->Next = l->Next;
+        l->Next = p;
+        return this;
+    }
+    else
+    {
+        cout << "K位置无法插入！" << endl;
+        return nullptr;
+    }
+}
+
+template <class T>
+List NodeS<T>::DeleteKth(int K)
+{
+    List p, l;
+    if (K == 1)
+    {
+        p = this;
+        p = p->Next;
+        // delete p; //不能在一个对象内部把自己删除
+        return p;
+    }
+    l = FindKth(K - 1);
+    if (l && l->Next)
+    {
+        p = l->Next;
+        l->Next = p->Next;
+        // delete p; //跳过待删除而后续还要用到的对象
+        return this;
+    }
+    else
+    {
+        cout << "位置K无法删除" << endl;
+        return nullptr;
+    }
+}
+
+template <class T>
+List NodeS<T>::Reverse()
+{
+    List pPre, pCur, pNext;
+    pPre = this;
+    pCur = pPre->Next;
+    pNext = nullptr;
+    while (pCur)
+    {
+        pNext = pCur->Next;
+        pCur->Next = pPre;
+        pPre = pCur;
+        pCur = pNext;
+    }
+    this->Next = nullptr;
+    return pPre;
+}
+
+template <class T>
+List NodeS<T>::Merge(List L1)
+{
+    List L = L1;
+    while (L->Next)
+        L = L->Next;
+    L->Next = this;
+    return L1;
+}
+
+template <class T>
+List NodeS<T>::sortByType()
+{
+    List p = this, pIH, pIE, pDH, pDE, pCH, pCE;
+    pIH = pIE = nullptr;
+    pDH = pDE = nullptr;
+    pCH = pCE = nullptr;
+    while (p)
+    {
+        if (p->tellType() == 1)
+        {
+            if (!pIH)
+            {
+                pIH = pIE = p;
+            }
+            else
+            {
+                pIE->Next = p;
+                pIE = p;
+            }
+        }
+        else if (p->tellType() == 2)
+        {
+            if (!pDH)
+            {
+                pDH = pDE = p;
+            }
+            else
+            {
+                pDE->Next = p;
+                pDE = p;
+            }
+        }
+        else
+        {
+            if (!pCH)
+            {
+                pCH = pCE = p;
+            }
+            else
+            {
+                pCE->Next = p;
+                pCE = p;
+            }
+        }
+        p = p->Next;
+    }
+    pIE->Next = pDH;
+    pDE->Next = pCH;
+    pCE->Next = nullptr;
+    return pIH;
+}
+
+template <class T>
+List NodeS<T>::Destroy()
+{
+    this->Next = nullptr;
+    return this;
+}
---
+++ Heterogeneous Chain/Heterogeneous Chain in C.cpp
@@ -0,0 +1,299 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum TYPE
+{
+    INT,
+    DOUBLE,
+    CHAR
+};
+
+typedef struct Node *List;
+struct Node
+{
+    void *Data;
+    enum TYPE type;
+    List Next;
+};
+
+List createEmpty();                                     //建立空链表
+void Output(List PtrL);                                 //遍历链表输出
+List FindKth(List PtrL, int K);                         //查找并指向第K个元素
+List Insert(List PtrL, int K, enum TYPE type, void *x); //在第K个位置插入类型为type的元素
+List DeleteKth(List PtrL, int K);                       //删除第K个位置的元素
+List Reverse(List PtrL);                                //逆置链表
+List Merge(List PtrL1, List PtrL2);                     //合并链表
+List sortByType(List PtrL);                             //按值的类型排序（先INT，再DOUBLE，后CHAR）
+void Destroy(List PtrL);                                //销毁链表
+
+int main()
+{
+    List PtrL1, PtrL2;
+    int int1 = 1, int2 = 2;
+    double double1 = 2020.11, double2 = 2020.22;
+    char ch1 = 'a', ch2 = 'b';
+    //建表
+    PtrL1 = createEmpty();
+    PtrL2 = createEmpty();
+    //插入
+    PtrL1 = Insert(PtrL1, 1, INT, &int1);
+    PtrL1 = Insert(PtrL1, 2, INT, &int2);
+    PtrL1 = Insert(PtrL1, 1, DOUBLE, &double1);
+    PtrL2 = Insert(PtrL2, 1, CHAR, &ch1);
+    PtrL2 = Insert(PtrL2, 2, CHAR, &ch2);
+    PtrL2 = Insert(PtrL2, 3, DOUBLE, &double2);
+    //输出
+    printf("插入后的结果：\n");
+    printf("PtrL1中元素：\n");
+    Output(PtrL1);
+    printf("PtrL2中元素：\n");
+    Output(PtrL2);
+
+    printf("\n");
+
+    // //删除
+    // PtrL1 = DeleteKth(PtrL1, 2);
+    // PtrL2 = DeleteKth(PtrL2, 2);
+    // //输出
+    // printf("删除后的结果：\n");
+    // printf("PtrL1中元素：\n");
+    // Output(PtrL1);
+    // printf("PtrL2中元素：\n");
+    // Output(PtrL2);
+
+    // printf("\n");
+
+    //逆置
+    PtrL1 = Reverse(PtrL1);
+    PtrL2 = Reverse(PtrL2);
+    //输出
+    printf("逆置后的结果：\n");
+    printf("PtrL1中元素：\n");
+    Output(PtrL1);
+    printf("PtrL2中元素：\n");
+    Output(PtrL2);
+
+    printf("\n");
+
+    //合并
+    PtrL1 = Merge(PtrL2, PtrL1);
+    //输出
+    printf("合并后的结果：\n");
+    printf("PtrL1中元素：\n");
+    Output(PtrL1);
+
+    printf("\n");
+
+    //排序
+    PtrL1 = sortByType(PtrL1);
+    //输出
+    printf("排序后的结果：\n");
+    printf("PtrL1中元素：\n");
+    Output(PtrL1);
+
+    printf("\n");
+
+    //销毁
+    Destroy(PtrL1);
+    PtrL1 = NULL;
+    //输出
+    printf("销毁后的结果：\n");
+    printf("PtrL1中元素：\n");
+    Output(PtrL1);
+
+    getchar();
+}
+
+List createEmpty()
+{
+    List PtrL = NULL;
+    return PtrL;
+}
+
+List FindKth(List PtrL, int K)
+{
+    List L = PtrL;
+    int i = 1;
+    while (L && i < K)
+    {
+        L = L->Next;
+        i++;
+    }
+    if (i == K)
+        return L;
+    else
+    {
+        printf("K位置无法查找！");
+        return NULL;
+    }
+}
+
+List Insert(List PtrL, int K, enum TYPE type, void *x)
+{
+    List p, l;
+    if (K == 1)
+    {
+        p = (List)malloc(sizeof(struct Node));
+        p->Data = x;
+        p->type = type;
+        p->Next = PtrL;
+        return p;
+    }
+    l = FindKth(PtrL, K - 1);
+    if (l)
+    {
+        p = (List)malloc(sizeof(struct Node));
+        p->Data = x;
+        p->type = type;
+        p->Next = l->Next;
+        l->Next = p;
+        return PtrL;
+    }
+    else
+    {
+        printf("K位置无法插入！\n");
+        return NULL;
+    }
+}
+
+void Output(List PtrL)
+{
+    List p = PtrL;
+    if (!PtrL)
+    {
+        printf("链表为空！");
+        return;
+    }
+    else
+    {
+        while (p)
+        {
+            if (p->type == INT)
+                printf("整型，值为%d\n", *(int *)p->Data);
+            else if (p->type == CHAR)
+                printf("字符型，值为%c\n", *(char *)p->Data);
+            else
+                printf("浮点型，值为%.2lf\n", *(double *)p->Data);
+
+            p = p->Next;
+        }
+        return;
+    }
+}
+
+List DeleteKth(List PtrL, int K)
+{
+    List p, l;
+    if (K == 1)
+    {
+        p = PtrL;
+        PtrL = PtrL->Next;
+        free(p);
+        return PtrL;
+    }
+    l = FindKth(PtrL, K - 1);
+    if (l && l->Next)
+    {
+        p = l->Next;
+        l->Next = p->Next;
+        free(p);
+        return PtrL;
+    }
+    else
+    {
+        printf("位置K无法删除！");
+        return NULL;
+    }
+}
+
+List Reverse(List PtrL)
+{
+    List pPre, pCur, pNext;
+    pPre = PtrL;
+    pCur = pPre->Next;
+    pNext = NULL;
+    while (pCur)
+    {
+        pNext = pCur->Next;
+        pCur->Next = pPre;
+        pPre = pCur;
+        pCur = pNext;
+    }
+    PtrL->Next = NULL;
+    PtrL = pPre;
+    return PtrL;
+}
+
+List Merge(List PtrL1, List PtrL2)
+{
+    List L = PtrL1;
+    while (L->Next)
+        L = L->Next;
+    L->Next = PtrL2;
+    return PtrL1;
+}
+
+List sortByType(List PtrL)
+{
+    List p = PtrL, pIH, pIE, pDH, pDE, pCH, pCE;
+    pIH = pIE = NULL;
+    pDH = pDE = NULL;
+    pCH = pCE = NULL;
+    while (p)
+    {
+        if (p->type == INT)
+        {
+            if (!pIH)
+            {
+                pIH = pIE = p;
+            }
+            else
+            {
+                pIE->Next = p;
+                pIE = p;
+            }
+        }
+        else if (p->type == DOUBLE)
+        {
+            if (!pDH)
+            {
+                pDH = pDE = p;
+            }
+            else
+            {
+                pDE->Next = p;
+                pDE = p;
+            }
+        }
+        else
+        {
+            if (!pCH)
+            {
+                pCH = pCE = p;
+            }
+            else
+            {
+                pCE->Next = p;
+                pCE = p;
+            }
+        }
+        p = p->Next;
+    }
+    pIE->Next = pDH;
+    pDE->Next = pCH;
+    pCE->Next = NULL;
+    PtrL = pIH;
+    return PtrL;
+}
+
+void Destroy(List PtrL)
+{
+    List p;
+    while (PtrL)
+    {
+        p = PtrL;
+        PtrL = PtrL->Next;
+        free(p);
+    }
+    return;
+}
