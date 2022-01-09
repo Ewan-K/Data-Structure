8738dacd6ccc6027008c8ec981b9e0e519361cbf
Ewan-K <2574017297@qq.com>
2022-01-09 12:45:04 +0800

    Set

---
+++ Set/Union_Find_Complex.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+#define MAXSIZE 100
+
+typedef int ElementType;
+typedef struct
+{
+    ElementType Data; // 如果能把所有数据映射到0~N-1，则可以直接用下标代表Data，不用开辟Data域，见Reduced
+    int Parent;
+} SetType;
+
+int Find(SetType S[], ElementType X)
+{
+    int i;
+    for (i = 0; i < MAXSIZE && S[i].Data != X; i++) // 找出X在不在集合中
+        ;
+    if (i > MAXSIZE)
+        return -1;
+    for (; S[i].Parent >= 0; i = S[i].Parent) // 找出X在哪个集合
+        ;
+    return i;
+}
+
+void Union(SetType S[], ElementType X1, ElementType X2)
+{
+    int Root1 = Find(S, X1);
+    int Root2 = Find(S, X2);
+    if (Root1 != Root2)
+        S[Root2].Parent = Root1;
+}
---
+++ Set/Union_Find_Reduced.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+#define MAXSIZE 100
+
+typedef int ElementType;
+typedef int SetName;                  // 用下标表示集合的名称
+typedef ElementType SetType[MAXSIZE]; // 这个写法很不好理解
+
+SetName Find(SetType S, ElementType X)
+{
+    for (; S[X] >= 0; X = S[X]) // 找出X在哪个集合
+        ;
+    return X;
+}
+void Union(SetType S, SetName Root1, SetName Root2)
+{
+    // 找到两个元素所属的Root1和Root2对应的不同集合后直接合并Root1和Root2
+    S[Root2] = Root1;
+}
