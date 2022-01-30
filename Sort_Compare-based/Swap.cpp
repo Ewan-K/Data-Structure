875af9436d0f47bb32b5d3bf6087de328fa4afe3
Ewan-K <2574017297@qq.com>
2022-01-30 17:33:09 +0800

    Bubble

--- Sort_Compare-based/Swap.cpp
+++ Sort_Compare-based/Swap.cpp
@@ -1,1 +1,26 @@
 // 冒泡、快排
+#include <iostream>
+using namespace std;
+
+#define MAXN 100
+
+int num[MAXN];
+int N;
+
+void bubble(int num[], int N)
+{
+}
+
+int main()
+{
+    cout << "Please input how many numbers?" << endl;
+    cin >> N;
+    cout << "Please input the numbers:" << endl;
+    for (int i = 0; i < N; i++)
+        cin >> num[i];
+    bubble(num, N);
+    for (int i = 0; i < N; i++)
+        cout << num[i] << ' ';
+
+    return 0;
+}
