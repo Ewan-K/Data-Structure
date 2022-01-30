585814f2635d6332712a6a936792abab60ab9f47
Ewan-K <2574017297@qq.com>
2022-01-30 18:22:17 +0800

    not important

--- Sort_Compare-based/Insert.cpp
+++ Sort_Compare-based/Insert.cpp
@@ -1,1 +1,26 @@
 // 直接插入、希尔
+#include <iostream>
+using namespace std;
+
+#define MAXN 100
+
+int num[MAXN];
+int N;
+
+void bubble(int num[], int N)
+{
+}
+
+int main()
+{
+    cout << "Please input how many numbers?" << endl;
+    cin >> N;
+    cout << "Please input the numbers:" << endl;
+    for (int i = 0; i < N; i++)
+        cin >> num[i];
+
+    for (int i = 0; i < N; i++)
+        cout << num[i] << ' ';
+
+    return 0;
+}
