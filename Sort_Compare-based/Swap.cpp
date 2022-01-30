5ebcc394492d3cd603c3e845d26a87e61cfb7433
Ewan-K <2574017297@qq.com>
2022-01-30 19:22:02 +0800

    bubble

--- Sort_Compare-based/Swap.cpp
+++ Sort_Compare-based/Swap.cpp
@@ -7,8 +7,22 @@ using namespace std;
 int num[MAXN];
 int N;
 
-void bubble(int num[], int N)
+void bubble(int num[], int N) // 最大的优势是可以用于链表排序
 {
+    for (int i = N - 1; i > 0; i--)
+    {
+        int flag = 0; // 如果中间某趟已经排好序了就不用继续
+        for (int j = 0; j < i; j++)
+        {
+            if (num[j] > num[j + 1])
+            {
+                flag = 1;
+                swap(num[j], num[j + 1]);
+            }
+        }
+        if (flag == 0)
+            break;
+    }
 }
 
 int main()
